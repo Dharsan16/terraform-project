37037b845b87f01dfe99a4ca82cfb7ae00edbb38
Dharsan16 <86907338+Dharsan16@users.noreply.github.com>
2026-03-30 18:24:37 +0530

    Delete main1.tf

--- main1.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "terraform-ec2-instance" {
-  ami           = "ami-0c3389a4fa5bddaad"   # Amazon Linux (us-east-1)
-  instance_type = "t2.micro"
-  key_name      = "test"
-
-  tags = {
-    Name = "Jenkins-Terraform-EC2"
-  }
-}
